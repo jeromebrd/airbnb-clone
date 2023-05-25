c8e9c0ecec0a3d31801098a8ec36585dba068376
Jerome <jerome.bourscheid@gmail.com>
2023-05-25 19:33:12 +0200

    add Date : 'react-date-range'

---
+++ src/app/search/page.tsx
@@ -0,0 +1,24 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
+export default function Search() {
+  return (
+    <div>
+      <Header />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ Stays for 5 number of guests</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in May</h1>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and beds</p>
+            <p className="button">More filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,64 @@
+'use client';
+import { Bars3Icon, GlobeAltIcon } from '@heroicons/react/24/outline';
+import {
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from '@heroicons/react/24/solid';
 import Image from 'next/image';
-import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid';
-import { GlobeAltIcon, Bars3Icon } from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker, Range, RangeKeyDict } from 'react-date-range';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [nbOfGuests, setNbOfGuests] = useState(1);
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const query = useSearchParams();
+  console.log(pathname);
+  console.log(query);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearchInput(e.target.value);
+  /* 
+    Selection date on the calendar, with startDate/endDate when user click on.
+    Param of function is rangesByKey => type RangeKeyDict (get by 'react-date-range') 
+    The type of startDate/endDate doesn't allow 'undefined' => condition with verification it's not 'undefine'
+  */
+  const handleSelect = (rangesByKey: RangeKeyDict) => {
+    const ranges = rangesByKey.selection; //selection is the "key" in the const SelectionRange.
+    if (ranges && ranges.startDate && ranges.endDate) {
+      setStartDate(ranges.startDate);
+      setEndDate(ranges.endDate);
+    }
+  };
+  // set params of DateRangePicker :
+  const selectionRange: Range = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
+  const resetInput = () => {
+    setSearchInput('');
+  };
+
+  // The function serves as a search function triggered when the user clicks on the button, sending the user's selected information to the '/search' page.
+  const search = () => {};
+
+  // ============================= return =======================================
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push('/')}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           fill
@@ -18,6 +70,8 @@ const Header = () => {
       {/* middle - search */}
       <div className="flex items-center rounded-full py-2 md:shadow-sm md:border-2">
         <input
+          value={searchInput}
+          onChange={handleChange}
           type="text"
           placeholder="Start your search"
           className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
@@ -34,6 +88,37 @@ const Header = () => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5 " />
+            <input
+              value={nbOfGuests}
+              onChange={(e) => setNbOfGuests(parseInt(e.target.value))}
+              min={1}
+              type="number"
+              className="w-12 pl-2 text-lg text-red-400 outline-none"
+            />
+          </div>
+          <div className="flex">
+            <button className="flex-grow text-gray-500" onClick={resetInput}>
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400" onClick={search}>
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
